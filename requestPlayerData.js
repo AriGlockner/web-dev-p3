a51fdd9343fe40f4c437a52594658ce3b4415c9a
Ari Glockner <ari@glockners.net>
2024-04-21 14:07:11 -0400

    Fixed data in table and removed files

--- requestPlayerData.js
+++ requestPlayerData.js
@@ -28,16 +28,16 @@ function extractData(data) {
     data.forEach(player => {
         const Name = player.entity_name;
         const PA = parseInt(player.pa);
-        const BA = player.ba;
-        const xBA = player.est_ba;
-        const BA_diff = BA - xBA;
-        const SLG = player.slg;
-        const xSLG = player.est_slg;
-        const SLG_diff = SLG - xSLG;
-        const wOBA = player.woba;
-        const xwOBA = player.est_woba;
-        const wOBA_diff = wOBA - xwOBA;
-        const luck = (BA_diff + SLG_diff + wOBA_diff) / 3;
+        const BA = parseFloat(player.ba).toFixed(3); //player.ba;
+        const xBA = parseFloat(player.est_ba).toFixed(3); //player.est_ba;
+        const BA_diff = parseFloat(BA - xBA).toFixed(3);
+        const SLG = parseFloat(player.slg).toFixed(3); //player.slg;
+        const xSLG = parseFloat(player.est_slg).toFixed(3); //player.est_slg;
+        const SLG_diff = parseFloat(SLG - xSLG).toFixed(3);
+        const wOBA = parseFloat(player.woba).toFixed(3); //player.woba;
+        const xwOBA = parseFloat(player.est_woba).toFixed(3); //player.est_woba;
+        const wOBA_diff = parseFloat(wOBA - xwOBA).toFixed(3);
+        const luck = parseFloat((BA_diff + SLG_diff + wOBA_diff) / 3).toFixed(3); //(BA_diff + SLG_diff + wOBA_diff) / 3;
 
         players.push({Name, PA, BA, xBA, BA_diff, SLG, xSLG, SLG_diff, wOBA, xwOBA, wOBA_diff, luck });
     });
